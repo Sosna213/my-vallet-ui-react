6b2c36180aad80c478fc02f9338899389b6ce3d8
Mateusz Sosnowski <sosnowskimateusz25@gmail.com>
2024-02-13 18:04:43 +0100

    Navbar rwd adding

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -2,59 +2,81 @@ import { Link, NavLink, useLocation } from "react-router-dom";
 import { sidebarLinks } from "../../constants";
 import { INavLink } from "../../types";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useState } from "react";
+import { Button } from "../ui/button";
 
 const LeftSidebar = () => {
   const { isAuthenticated } = useAuth0();
   const { pathname } = useLocation();
+
+  const [isCollapsed, setIsCollapsed] = useState(true);
   const links = isAuthenticated
     ? sidebarLinks
     : sidebarLinks.filter((link) => link.needsAuthentication != true);
 
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <div className="h-full flex flex-col">
-      <div className="flex gap-3 m-3">
-        <Link to="/" className="items-center">
+    <div className={`h-full flex flex-col min-w-[72px]`}>
+      <div className="flex gap-3 m-3 items-center">
+        <Link to="/">
           <img
-            className="w-14 dark:invert ml-5"
-            src={"/assets/logo.svg"}
+            className="w-8 md:w-14 dark:invert ml-2 md:ml-5"
+            src="/assets/logo.svg"
             alt="logo"
           />
         </Link>
-        <Link to="/" className="flex items-center">
-          <span className=" font-bold text-2xl hidden sm:flex">My wallet</span>
-        </Link>
+        {!isCollapsed && (
+          <Link to="/" className="hidden lg:flex">
+            <span className="font-bold text-2xl">My wallet</span>
+          </Link>
+        )}
       </div>
-      <nav className="leftsidebar sm:hidden lg:inline">
-        <ul className="flex flex-col gap-6">
-          {links.map((link: INavLink) => {
-            const isActive = pathname === link.route;
+      <div
+        className={
+          !isCollapsed ? "fixed lg:static bg-background z-50 h-full" : ""
+        }
+      >
+        <nav
+          className={`leftsidebar  ${!isCollapsed ? "lg:min-w-[260px]" : ""}`}
+        >
+          <div className="flex justify-center mb-3">
+            <Button variant={"ghost"} onClick={toggleSidebar}>
+              {isCollapsed ? "☰" : "✕"}
+            </Button>
+          </div>
 
-            return (
-              <li
-                key={link.label}
-                className={`leftsidebar-link group ${
-                  isActive && "dark:bg-slate-800/50 bg-slate-100/50"
-                }`}
-              >
-                <NavLink
-                  to={link.route}
-                  className="flex gap-4 items-center p-4"
+          <ul className="flex flex-col gap-6">
+            {links.map((link: INavLink) => {
+              const isActive = pathname === link.route;
+              return (
+                <li
+                  key={link.label}
+                  className={`leftsidebar-link group ${
+                    isActive && "dark:bg-slate-800/50 bg-slate-100/50"
+                  }`}
                 >
-                  <img
-                    src={link.imgURL}
-                    alt={link.label}
-                    width={24}
-                    className={`group-hover:invert-white dark:invert ${
-                      isActive && "invert-white"
-                    }`}
-                  />
-                  {link.label}
-                </NavLink>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+                  <NavLink
+                    to={link.route}
+                    className="flex gap-2 md:gap-4 items-center p-4"
+                  >
+                    <img
+                      src={link.imgURL}
+                      alt={link.label}
+                      className={`w-4 md:w-6 group-hover:invert-white dark:invert ${
+                        isActive && "invert-white"
+                      }`}
+                    />
+                    {isCollapsed ? null : link.label}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
